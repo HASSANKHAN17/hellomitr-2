e9129f284ab0a8c8481d8620ef6127e65508e11e
sharjeelsk <54668506+sharjeelsk@users.noreply.github.com>
2022-02-17 16:52:22 +0530

    checkout complete

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -50,7 +50,68 @@ function Checkout() {
     <div>
         <Header />
         <SubHeader />
-        <div className="shadow-sm checkout">
+        <div className='row m-auto checkout justify-content-between'>
+          <div className="col-8">
+
+          <div className="shadow-sm paymentdetails">
+            <h1>Delivery Details</h1>
+            <p>Delivery Address</p>
+            <div className="m-auto row justify-content-between">
+              <div onClick={()=>setSelected(0)} className={selected===0?"col-5 pg active":"col-5 pg"}>
+                <h5>John Dow</h5>
+                <p>93 / Parshwakutir Perin Nariman St Bazar Gate Street Fort, Mumbai,Mumbai,400001,India</p>
+              </div>
+
+              <div onClick={()=>setSelected(1)} className={selected===1?"col-5 pg active":"col-5 pg"}>
+                <h5>Andrew Phase</h5>
+                <p>Shop No.3 Swami Bldg Sec 5 Ambedkar Rd Charkop Kandivli, Mumbai,Adoni,400067,India</p>
+              </div>
+
+              <div onClick={()=>setSelected(1)} className={selected===1?"col-5 pg active":"col-5 pg"}>
+                <h5>Macdue Rad</h5>
+                <p>36  Anand Sagar Nr Amar Gyan Estate Thane , Mumbai,Ahmedabad,400607,India</p>
+              </div>
+
+              <div onClick={()=>setSelected(1)} className={selected===1?"col-5 pg active":"col-5 pg"}>
+                <h5>Vanessa Fisher</h5>
+                <p>7  Khassamwala Bldg bhandari Street Mandvi, Mumbai,Akola,400003,India</p>
+              </div>
+
+
+              <div onClick={()=>setSelected(1)} className={selected===1?"col-5 pg active":"col-5 pg"}>
+                <h5>Marshall Soto</h5>
+                <p>Shop No.3 Swami Bldg Sec 5 Ambedkar Rd Charkop Kandivli, Mumbai,Adoni,400067,India</p>
+              </div>
+
+              <div onClick={()=>setSelected(1)} className={selected===1?"col-5 pg active":"col-5 pg"}>
+                <h5>Marco Parks</h5>
+                <p>Shop No.3 Swami Bldg Sec 5 Ambedkar Rd Charkop Kandivli, Mumbai,Adoni,400067,India</p>
+              </div>
+
+            </div>
+            <Button onClick={()=>selected===1?openPayModal():null} className="btn" variant="contained" fullWidth>Pay now</Button>
+            </div>
+
+
+            <div className="shadow-sm paymentdetails">
+            <h1>Delivery Mode</h1>
+            <p>Choose Payment Method for your Order</p>
+            <div className="m-auto row justify-content-between">
+              <div onClick={()=>setSelected(0)} className={selected===0?"col-5 pg active":"col-5 pg"}>
+                <h5>Standard</h5>
+                <p>Free</p>
+              </div>
+
+              <div onClick={()=>setSelected(1)} className={selected===1?"col-5 pg active":"col-5 pg"}>
+                <h5>Instant</h5>
+                <p>Additional INR499/- Only</p>
+              </div>
+
+            </div>
+            </div>
+
+
+            <div className="shadow-sm paymentdetails">
             <h1>Payment Details</h1>
             <p>Choose Payment Method for your Order</p>
             <div className="m-auto row justify-content-between">
@@ -66,7 +127,39 @@ function Checkout() {
 
             </div>
             <Button onClick={()=>selected===1?openPayModal():null} className="btn" variant="contained" fullWidth>Pay now</Button>
+            </div>
+
+
+
+
+
+          </div>
+
+          <div className="shadow-sm col-3 totaldiv">
+              <h5>Your order</h5>
+              <div className="row m-auto justify-content-between">
+                <p className="greytext">Subtotal:</p>
+                <p><b>INR 1523</b></p>
+              </div>
+
+              <div className="row m-auto justify-content-between">
+                <p className="greytext">Shipping:</p>
+                <p><b>INR 23</b></p>
+              </div>
+
+              <div className="row m-auto justify-content-between">
+                <p className="greytext">Discount:</p>
+                <p><b>INR 1623</b></p>
+              </div>
+
+              <hr />
+              <p className="total">INR 1623</p>
+          </div>
         </div>
+
+
+
+       
         {/* <Footer /> */}
 
     </div>
